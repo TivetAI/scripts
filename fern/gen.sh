544c248b55ed83893d596a5679848a4e151315b2
XxJipow <jiprcroyale@gmail.com>
2024-12-24 15:50:00 +0000

    add fern

---
+++ fern/gen.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euf -o pipefail
+
+FERN_GROUP=runtime ./scripts/fern/gen_inner.sh &
+FERN_GROUP=full ./scripts/fern/gen_inner.sh &
+wait
+
+# Generate APIs
+./site/scripts/generateApi.js
+
+# Update lockfile
+yarn
+
